3226eebe6690d9177006cdda8f7250b1431e5929
stefuthomas <stefanos.thomas@metropolia.fi>
2023-08-24 21:45:19 +0300

    initial commit

---
+++ Python moduuli 2 teht 6.py
@@ -0,0 +1,11 @@
+import random
+
+numero1 = (random.randint(0,9))
+numero2 = (random.randint(0,9))
+numero3 = (random.randint(0,9))
+number1 = (random.randint(1,6))
+number2 = (random.randint(1,6))
+number3 = (random.randint(1,6))
+number4 = (random.randint(1,6))
+print("Tässä arvottu kolmen numeron koodi: " + str(numero1) + str(numero2) + str(numero3))
+print("Tässä arvottu neljän numeron koodi: " + str(number1) + str(number2) + str(number3) + str(number4))
