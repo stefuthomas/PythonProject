3e2c8be59cd5f26cc1283c876a55be1acc8ef28b
stefuthomas <stefanos.thomas@metropolia.fi>
2023-08-24 17:23:09 +0300

    Korjattu väärä kertolasku

--- Python moduuli 2 teht 4.py
+++ Python moduuli 2 teht 4.py
@@ -2,7 +2,7 @@ ensimmäinenLuku = int(input("Anna ensimmäinen luku: "))
 toinenLuku = int(input("Anna toinen luku: "))
 kolmasLuku = int(input("Anna kolmas luku: "))
 summa = int(ensimmäinenLuku + toinenLuku + kolmasLuku)
-tulo = int(ensimmäinenLuku + toinenLuku + kolmasLuku)
+tulo = int(ensimmäinenLuku * toinenLuku * kolmasLuku)
 keskiarvo = int(ensimmäinenLuku + toinenLuku + kolmasLuku)/3
 print("tässä summa: "+str(summa))
 print("tässä tulo: "+str(tulo))
