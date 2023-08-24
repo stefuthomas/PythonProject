f45eb8fa57b23114300a74ee9d5808bc50ee2c15
stefuthomas <stefanos.thomas@metropolia.fi>
2023-08-24 15:04:38 +0300

    Initial Commit

---
+++ Python moduuli 2 teht 4.py
@@ -0,0 +1,9 @@
+ensimmäinenLuku = int(input("Anna ensimmäinen luku: "))
+toinenLuku = int(input("Anna toinen luku: "))
+kolmasLuku = int(input("Anna kolmas luku: "))
+summa = int(ensimmäinenLuku + toinenLuku + kolmasLuku)
+tulo = int(ensimmäinenLuku + toinenLuku + kolmasLuku)
+keskiarvo = int(ensimmäinenLuku + toinenLuku + kolmasLuku)/3
+print("tässä summa: "+str(summa))
+print("tässä tulo: "+str(tulo))
+print("tässä keskiarvo: "+str(keskiarvo))
