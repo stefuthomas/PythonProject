9ba8f447687ae35714e2188aa14814e54274e02e
stefuthomas <stefanos.thomas@metropolia.fi>
2023-08-28 10:16:33 +0300

    tehty muutoksia koodiin

--- Python moduuli 3 teht 1.py
+++ Python moduuli 3 teht 1.py
@@ -1,19 +1,7 @@
 kuha = int(input("Anna kuhan pituus(cm): "))
-if kuha>=37:
-    print("kuha on sallitun mittainen!")
-elif kuha==36:
-    print("kuha on sentin alimittainen, laske se takaisin veteen!")
-elif kuha==35:
-    print("kuha on 2 senttiä alimittainen, laske se takaisin veteen!")
-elif kuha==34:
-    print("kuha on 3 senttiä alimittainen laske se takaisin veteen!")
-elif kuha==33:
-    print("kuha on 4 senttiä alimittainen laske se takaisin veteen!")
-elif kuha==32:
-    print("kuha on 5 senttiä alimittainen laske se takaisin veteen!")
-elif kuha==31:
-    print("kuha on 6 senttiä alimittainen laske se takaisin veteen!")
-elif kuha==30:
-    print("kuha on 7 senttiä alimittainen laske se takaisin veteen!")
+raja = (37)
+
+if kuha < 37:
+    print ("Kuha on alimittainen, laske se veteen! Kuhan sallitusta pituudesta puuttuu: " + str(raja - kuha) + ' senttimetriä' )
 else:
-    print("alle 30 senttiset kuhat eivät ole lähelläkään sallittua mittaa! Sopiva pituus on 37 cm! Laske se takaisin veteen.")
+    print("Kuha on sallitun mittainen!")
