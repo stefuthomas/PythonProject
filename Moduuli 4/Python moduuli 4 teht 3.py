fe91a33d58c7c784be672af5a27cfb3b8987abd1
stefuthomas <stefanos.thomas@metropolia.fi>
2023-09-01 14:05:50 +0300

    initial commit

--- Moduuli 4/Python moduuli 4 teht 3.py
+++ Moduuli 4/Python moduuli 4 teht 3.py
@@ -1,9 +1,14 @@
-luku_lista = []
+p = int(-1)
+s = int(1)
+numero = int(0)
+syöte = '0'
 
-luku = str(input('Syötä luku, tai lopeta painamalla "enter": '))
-while luku!='':
-    luku_lista.append(luku)
-    luku = str(input('Syötä luku, tai lopeta painamalla "enter": '))
-
-print("Pienin syötetty luku =",min(luku_lista))
-print("Suurin syötetty luku =",max(luku_lista))
+while syöte!='':
+    syöte = (input('Anna luku: '))
+    if syöte!='':
+        numero = int(syöte)
+        if numero > s:
+            s = numero
+        elif numero == -1 or numero < p:
+            p = numero
+print('Suurin numero on ' + str(s) + ' Pienin numero on ' + str(p))
