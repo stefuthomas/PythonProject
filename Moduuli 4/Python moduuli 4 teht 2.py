5d8f5878c7ca56234232c803563b6a97893f3d9c
stefuthomas <stefanos.thomas@metropolia.fi>
2023-09-01 15:15:14 +0300

    Muokattu komentoa

--- Moduuli 4/Python moduuli 4 teht 2.py
+++ Moduuli 4/Python moduuli 4 teht 2.py
@@ -1,6 +1,6 @@
-tuuma = int(input('Anna tuumat: '))
+tuuma = float(input('Anna tuumat: '))
 sentti = 2.54
 while tuuma>0:
     print(f'tuumat sentteinä: {tuuma*sentti}')
-    tuuma = int(input('Anna tuumat: '))
+    tuuma = float(input('Anna tuumat: '))
 print("lopetan toimintani")
