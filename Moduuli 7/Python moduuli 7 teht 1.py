d24e9d9e9a2d3b8497a0f4df1da60946c2eaafdf
stefuthomas <stefanos.thomas@metropolia.fi>
2023-09-11 10:36:07 +0300

    Initial commit

---
+++ Moduuli 7/Python moduuli 7 teht 1.py
@@ -0,0 +1,7 @@
+vuodenajat = ('talvi', 'talvi', 'kevät', 'kevät', 'kevät', 'kesä', 'kesä', 'kesä', 'syksy', 'syksy', 'syksy', 'talvi')
+numero = int(input('Anna kuukauden numero: '))
+if 1 <= numero <= 12:
+    tulos = vuodenajat[numero-1]
+    print(f'kuukauden {numero}. vuodenaika on {tulos}')
+else:
+    print('Kuukauden numero ei ole välillä 1-12.')
