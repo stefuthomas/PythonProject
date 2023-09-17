57de27521c51978c29a1347ff5567a88457afa94
stefuthomas <stefanos.thomas@metropolia.fi>
2023-09-17 12:36:17 +0300

    Initial commit

---
+++ Moduuli 8/Python moduuli 8 teht 2.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+def hakeminen(koodi):
+    sql = " SELECT name, type, COUNT(*) FROM airport "
+    sql += " WHERE iso_country ='" + koodi + "'"
+    sql += " GROUP BY type; "
+    print(sql)
+    kursori = yhteys.cursor()
+    kursori.execute(sql)
+    tulos = kursori.fetchall()
+    if kursori.rowcount > 0:
+        for rivi in tulos:
+            print(rivi)
+
+
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='yAXDyg2T',
+         autocommit=True
+         )
+koodi = input('Anna Maa-koodi: ')
+hakeminen(koodi)
