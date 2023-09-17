602e6600837d669b2d3bc200cd69c87b8e3f2115
stefuthomas <stefanos.thomas@metropolia.fi>
2023-09-17 20:46:32 +0300

    Initial commit

---
+++ Moduuli 8/Python moduuli 8 teht 3.py
@@ -0,0 +1,38 @@
+import mysql.connector
+
+from geopy.distance import geodesic
+
+def distance_calculating(koodi, koodi2):
+    kursori = yhteys.cursor()
+    kursori.execute(f"SELECT longitude_deg, latitude_deg FROM airport WHERE ident = '{koodi1}'")
+    tulos1 = kursori.fetchone()
+    kursori.close()
+
+    kursori = yhteys.cursor()
+    kursori.execute(f"SELECT longitude_deg, latitude_deg FROM airport WHERE ident = '{koodi2}'")
+    tulos2 = kursori.fetchone()
+    kursori.close()
+
+    if tulos1 and tulos2:
+        coords1 = (tulos1[1], tulos1[0])
+        coords2 = (tulos2[1], tulos2[0])
+
+        etäisyys = geodesic(coords1, coords2).kilometers
+
+        return etäisyys
+    else:
+        return None
+yhteys = mysql.connector.connect(
+         host='127.0.0.1',
+         port= 3306,
+         database='flight_game',
+         user='root',
+         password='yAXDyg2T',
+         autocommit=True
+         )
+koodi1 = input('Anna ensimmäinen ICAO-koodi: ')
+koodi2 = input('Anna toinen ICAO-koodi: ')
+
+etäisyys = distance_calculating(koodi1, koodi2)
+
+print(f'Lentokentän {koodi1} ja {koodi2} on {etäisyys:.2f} kilometriä.')
